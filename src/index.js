6f87cc41c016182ac6b3f726823228392116453e
KelvinMitaki <mitakikelvin1@gmail.com>
2020-05-15 11:56:29 +0300

    added client to server communication and back

--- src/index.js
+++ src/index.js
@@ -9,7 +9,16 @@ const io = socketio(server);
 
 app.use(express.static(path.join(__dirname, "../public")));
 
-io.on("connection", () => console.log("new web server connection"));
+let count = 0;
+
+io.on("connection", (socket) => {
+  console.log("new web server connection");
+  socket.emit("updateCount", count);
+  socket.on("increment", () => {
+    count++;
+    io.emit("updateCount", count);
+  });
+});
 
 server.listen(process.env.PORT, () =>
   console.log(`Server started on port ${process.env.PORT}`)
