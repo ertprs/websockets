4d70c62394ccbaed9161d847f6823ae47261b364
KelvinMitaki <mitakikelvin1@gmail.com>
2020-05-15 11:17:50 +0300

    added socket io for new user joining

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-const express = require("express");
+const http = require("http");
 const path = require("path");
+const express = require("express");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+io.on("connection", () => console.log("new web server connection"));
 
-app.listen(process.env.PORT, () =>
+server.listen(process.env.PORT, () =>
   console.log(`Server started on port ${process.env.PORT}`)
 );
